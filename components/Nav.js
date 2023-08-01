2528d5fb4d3e148dd107de47209f01e567c2ce38
Honeybadger <rajimoshood200482@gmail.com>
2023-08-01 17:56:34 +0100

    updated transition

--- components/Nav.js
+++ components/Nav.js
@@ -44,7 +44,7 @@ const Nav = () => {
             <Link
               className={`${
                 link.path === pathname && "text-accent"
-              } relative flex items-center group hover:text-accent`}
+              } relative flex items-center group hover:text-accent transition-all duration-300`}
               href={link.path}
               key={index}
             >

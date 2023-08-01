ab4a0656f1e45af72fb9e75c8e0ae557b613ac78
Honeybadger <rajimoshood200482@gmail.com>
2023-08-01 11:27:40 +0100

    updated background

--- components/Layout.js
+++ components/Layout.js
@@ -15,7 +15,11 @@ import TopLeftImg from "../components/TopLeftImg";
 
 const Layout = ({ children }) => {
   return (
-    <div className={"page"}>
+    <div
+      className={
+        "page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative"
+      }
+    >
       <TopLeftImg />
       <Nav />
       <Header />

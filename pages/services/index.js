494789a8433517dd409cf62a916465778a4b3bc1
Honeybadger <rajimoshood200482@gmail.com>
2023-08-01 10:22:03 +0100

    updated services to default

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,3 +1,46 @@
+// icons
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+} from "react-icons/rx";
+
+// Service Data
+export const serviceData = [
+  {
+    icon: <RxCrop />,
+    title: "Branding",
+    description:
+      "lorem ipsum sum dummy text that will be changed later in the course od editing.",
+  },
+  {
+    icon: <RxPencil2 />,
+    title: "Design",
+    description:
+      "lorem ipsum sum dummy text that will be changed later in the course od editing.",
+  },
+  {
+    icon: <RxDesktop />,
+    title: "Development",
+    description:
+      "lorem ipsum sum dummy text that will be changed later in the course od editing.",
+  },
+  {
+    icon: <RxReader />,
+    title: "Copywriting",
+    description:
+      "lorem ipsum sum dummy text that will be changed later in the course od editing.",
+  },
+  {
+    icon: <RxRocket />,
+    title: "SEO",
+    description:
+      "lorem ipsum sum dummy text that will be changed later in the course od editing.",
+  },
+];
+
 const Services = () => {
   return <div>Services</div>;
 };

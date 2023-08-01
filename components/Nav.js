9f3f26f41c93f453c121d6e121639ac8a7119960
Honeybadger <rajimoshood200482@gmail.com>
2023-08-01 09:42:56 +0100

    updated testimonial slider and other default datas

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,27 @@
-//  links
-const links = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+//icons
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from "react-icons/hi";
+
+//  links (Nav Data)
+export const navData = [
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];

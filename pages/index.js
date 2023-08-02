88574c86ee888ecdd5a84c83eb6585ce52c71fef
Honeybadger <rajimoshood200482@gmail.com>
2023-08-02 12:38:33 +0100

    corrected the bug to render particles and avatar

--- pages/index.js
+++ pages/index.js
@@ -60,6 +60,12 @@ const Home = () => {
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         {/* bg img */}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* particle */}
+        <div>Particles</div>
+        {/* avatar img */}
+        <div>
+          <Avatar />
+        </div>
       </div>
     </div>
   );

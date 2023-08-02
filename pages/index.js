a60d03e44bde3489dcdb823ca4b831b9de565c59
Honeybadger <rajimoshood200482@gmail.com>
2023-08-02 08:08:14 +0100

    created landing page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,44 @@
+// next image
+import Image from "next/image";
+
+//  components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+// framer motion
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <h1 className="h1">
+            Transforming Ideas <br /> Into{" "}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          {/* subtitle */}
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+            Logical and results-driven Web Developer committed to creating and
+            enhancing user-centric websites and web 3.0 projects for diverse
+            business goals.
+          </p>
+          {/* btn */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectBtn />
+          </div>
+        </div>
+      </div>
+      {/* image */}
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;

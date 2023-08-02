33035d93d4c9195e1d28c1254de6f7ce28296b71
Honeybadger <rajimoshood200482@gmail.com>
2023-08-02 11:12:54 +0100

    added motion effects

--- pages/index.js
+++ pages/index.js
@@ -19,20 +19,41 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
-          </h1>
+          </motion.h1>
           {/* subtitle */}
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
             Logical and results-driven Web Developer committed to creating and
             enhancing user-centric websites and web 3.0 projects for diverse
             business goals.
-          </p>
+          </motion.p>
           {/* btn */}
           <div className="flex justify-center xl:hidden relative">
             <ProjectBtn />
           </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectBtn />
+          </motion.div>
         </div>
       </div>
       {/* image */}

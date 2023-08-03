92980a4ade978e4346e449a9b56c9df38e975e27
Honeybadger <rajimoshood200482@gmail.com>
2023-08-03 11:38:13 +0100

    updated avater to my picture

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-        src={"/avatar.png"}
+        src={"/my_passport.png"}
         width={737}
         height={678}
         alt="avatar"

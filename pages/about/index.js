2b73375ca77f22343be0b6363b58afbb20dbe24c
Honeybadger <rajimoshood200482@gmail.com>
2023-08-03 21:32:33 +0100

    changed the avatar to dif aspect ratio

--- pages/about/index.js
+++ pages/about/index.js
@@ -114,7 +114,7 @@ const About = () => {
         initial="hidden"
         animate="show"
         exit="hidden"
-        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+        className="hidden xl:flex absolute bottom-0 -left-[200px]"
       >
         <Avatar />
       </motion.div>

058c2b7239933a70f3f21fd126a835baf3a16204
Honeybadger <rajimoshood200482@gmail.com>
2023-08-04 20:03:11 +0100

    replaced titles and added addition transition

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -90,13 +90,15 @@ const WorkSlider = () => {
                       <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
                         <div className="flex items-center gap-x-2 text-[13px] tracking[0.2em]">
                           {/* title part 1 */}
-                          <div className="delay-100">title part 1</div>
+                          <div className="delay-100">LIVE</div>
                           {/* title part 2 */}
                           <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                            title part 2
+                            PROJECT
                           </div>
                           {/* icon */}
-                          <div>icon</div>
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            <BsArrowRight />
+                          </div>
                         </div>
                       </div>
                     </div>

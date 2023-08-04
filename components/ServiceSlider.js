ab84f70e2117498039a642fa24c64b1d3561ece3
Honeybadger <rajimoshood200482@gmail.com>
2023-08-04 13:13:52 +0100

    created slider menu

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -15,6 +15,10 @@ import {
   RxRocket,
   RxArrowTopRight,
 } from "react-icons/rx";
+
+// import required modules
+import { FreeMode, Pagination } from "swiper";
+
 // data
 const serviceData = [
   {
@@ -45,7 +49,42 @@ const serviceData = [
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {serviceData.map((item, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="bg-[rgba(65, 47, 123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89, 65, 169, 0.15)] transition-all duration-300">
+              {/* icon */}
+              <div className="text-4xl text-accent mb-4">{item.icon}</div>
+              {/* title & description */}
+              <div>
+                <div>{item.title}</div>
+                <p>{item.description}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;

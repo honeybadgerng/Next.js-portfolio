59b66dd5d905f9ac92321234cef7de48fab908d0
Honeybadger <rajimoshood200482@gmail.com>
2023-08-04 16:36:43 +0100

    created workslider from service slide

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -44,8 +44,45 @@ export const workSlides = {
   ],
 };
 
+// import swiper react component
+import { Swiper, SwiperSlide } from "swiper/react";
+
+//
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
+
+// icons
+import { BsArrowRight } from "react-icons/bs";
+
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {workSlides.slides.map((item, index) => {
+        return <SwiperSlide key={index}>slide</SwiperSlide>;
+      })}
+    </Swiper>
+  );
 };
 
 export default WorkSlider;

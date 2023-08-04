8d0e2a5dd5d99b5dbb763cab784793ac015f9eaa
Honeybadger <rajimoshood200482@gmail.com>
2023-08-05 00:27:54 +0100

    started contact form

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,29 @@
+// components
+import Circles from "/components/Circles";
+
+// icons
+import { BsArrowRight } from "react-icons/bs";
+
+// framer motion
+import { motion } from "framer-motion";
+
+// variant
+import { fadeIn } from "../../variants";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="h-full bg-primary/30">
+      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+        {/* text & form */}
+        <div className="flex flex-col w-full max-w-[700px]">
+          {/* text */}
+          <h2 className="h2 text-center mb-12">
+            Let's <span className="text-accent">connect</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;

8d8ecf3a09754559288092a6f8ff89074e5af879
Honeybadger <rajimoshood200482@gmail.com>
2023-08-04 19:56:48 +0100

    created transition with delay

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -76,7 +76,7 @@ const WorkSlider = () => {
               {slide.images.map((image, index) => {
                 return (
                   <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
-                    <div className="flex items-center justify-center relative overflow-hidden">
+                    <div className="flex items-center justify-center relative overflow-hidden group">
                       {/* image */}
                       <Image
                         src={image.path}
@@ -88,11 +88,13 @@ const WorkSlider = () => {
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       {/* title */}
                       <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                        <div className="flex items-center gap-x-2">
+                        <div className="flex items-center gap-x-2 text-[13px] tracking[0.2em]">
                           {/* title part 1 */}
-                          <div>title part 1</div>
+                          <div className="delay-100">title part 1</div>
                           {/* title part 2 */}
-                          <div>title part 2</div>
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            title part 2
+                          </div>
                           {/* icon */}
                           <div>icon</div>
                         </div>

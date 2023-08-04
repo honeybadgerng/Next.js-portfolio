4172cfcbb0d4e363cdda647e6b82c926b1bcb27b
Honeybadger <rajimoshood200482@gmail.com>
2023-08-04 20:08:05 +0100

    added key{index}

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -75,7 +75,10 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-                  <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+                  <div
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                    key={index}
+                  >
                     <div className="flex items-center justify-center relative overflow-hidden group">
                       {/* image */}
                       <Image

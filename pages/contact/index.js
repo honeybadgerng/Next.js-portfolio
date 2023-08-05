169abd8922220515be34e5232f2c64575328551a
Honeybadger <rajimoshood200482@gmail.com>
2023-08-05 01:06:58 +0100

    added motion effect

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -17,11 +17,23 @@ const Contact = () => {
         {/* text & form */}
         <div className="flex flex-col w-full max-w-[700px]">
           {/* text */}
-          <h2 className="h2 text-center mb-12">
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 text-center mb-12"
+          >
             Let's <span className="text-accent">connect</span>
-          </h2>
+          </motion.h2>
           {/* form */}
-          <form className="flex-1 flex flex-col gap-6 w-full mx-auto">
+          <motion.form
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+          >
             {/* input group */}
             <div className="flex gap-x-6 w-full">
               <input type="text" placeholder="name" className="input" />
@@ -35,7 +47,7 @@ const Contact = () => {
               </span>
               <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
             </button>
-          </form>
+          </motion.form>
         </div>
       </div>
     </div>

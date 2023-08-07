eb85c4f2ce20fe5d687194ad9c9bc06b1895070d
Honeybadger <rajimoshood200482@gmail.com>
2023-08-07 20:39:54 +0100

    added yarn-decentralized-social-media-app.png path

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -33,7 +33,7 @@ export const workSlides = {
         },
         {
           title: "title",
-          path: "/thumb2.jpg",
+          path: "/yarn-decentralized-social-media-app.png",
         },
         {
           title: "title",

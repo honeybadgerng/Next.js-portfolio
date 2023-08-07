b1d939125ccb34af05c55003f7fcae0bb67caf92
Honeybadger <rajimoshood200482@gmail.com>
2023-08-07 20:32:20 +0100

    added rjbxclusive.png

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -13,7 +13,7 @@ export const workSlides = {
         },
         {
           title: "title",
-          path: "/thumb4.jpg",
+          path: "/rjbxclusive.png",
         },
         {
           title: "title",
